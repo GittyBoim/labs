8bb8e4612856bdd4ed0e16f76e289829588e674f
Your Name <name@name.io>
2024-03-27 22:14:31 +0000

    Update

--- test/wallet/walletFuzzTest.t.sol
+++ test/wallet/walletFuzzTest.t.sol
@@ -12,20 +12,56 @@ contract WalletFuzzTest is Test {
         w = new Wallet();
     }
 
-    function testdeposit(uint256 sum) public {
-        vm.startPrank(vm.addr(1));
-        vm.deal(vm.addr(1), sum);
-        payable(address(w)).transfer(sum);
-        assertEq(w.getBalance(), sum);
-        vm.stopPrank();
+    function testAddOwner(address owner) public {
+        w.addOwner(owner);
+        assertEq(w.countOwners(), 1);
+        assertEq(w.owners(owner), true);
     }
 
-    function testWithdraw(uint256 sum) public {
-        //w.addOwner(vm.addr(1));
-        //vm.startPrank(vm.addr(1));
+    function testAddMoreThanThreeOwners(address owner) public {
+        w.addOwner(vm.addr(1));
+        w.addOwner(vm.addr(2));
+        w.addOwner(vm.addr(3));
+        vm.expectRevert('There are already 3 owners cant add owner');
+        w.addOwner(owner);
+    }
+
+    function  testDeleteOwner(address owner) public {
+        w.addOwner(owner);
+        assertEq(w.owners(owner), true);
+        assertEq(w.countOwners(), 1);
+        w.deleteOwner(owner);
+        assertEq(w.owners(owner), false);
+        assertEq(w.countOwners(), 0);
+    }
+
+    function testDeleteNotExistsUser(address owner) public  {
+        vm.expectRevert('Owner not exists');
+        w.deleteOwner(owner);
+    }
+
+
+    function testDeposit(uint256 sum, address owner) public {
+        vm.prank(owner);
+        vm.deal(owner, sum);
+        payable(address(w)).transfer(sum);
+        assertEq(w.getBalance(), sum);
+    }
+
+    function testWithdraw(address owner, uint256 sum) public {
+        w.addOwner(owner);
+        vm.deal(owner, sum);
+        vm.startPrank(owner);
+        payable(address(w)).transfer(sum);
+        uint balance = w.getBalance();
+        assertEq(balance, sum);
+        //vm.startPrank(owner);
+        w.withdraw(sum);
+        vm.stopPrank();
+        assertEq(w.getBalance(), balance -sum);
         //vm.deal(payable(address(w)), sum);
         //w.withdraw(sum);
         //assertEq(w.getBalance(), sum);
         //vm.stopPrank();
-    }
+   }
 }
