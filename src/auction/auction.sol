48e1313532e89d809dcb154febbbf08d12984b5e
GittyBoim <g0556705220@gmail.com>
2024-05-09 16:31:53 +0300

    update

---
+++ src/auction/auction.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+import "oz/ERC721/ERC721.sol";
+import "oz/ERC721/IERC721.sol";
+import "oz/ERC20/ERC20.sol";
+
+
+contract Action {
+
+    IERC721 NFT;
+    IERC20 token;
+    
+    address owner;
+    uint public tokenId;
+    address seller;
+    address public maxBidder;
+    uint public maxBid;
+    bool public start;
+    uint public end;
+
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier isOwner {
+        require(msg.sender == owner, "only owaner access");
+        _;
+    }
+
+    function startAuction(address _NFT, uint _tokenId, address _token, uint initialBid, uint _end, address _seller) public isOwner {
+
+        require(NFT.ownerOf(tokenId) == seller, "msg.sender not tokenId owner");
+        require(_end < block.timestamp, "invalid end date");
+        require(!start, "Already there is auction");
+
+        start = true;
+        NFT = IERC721(_NFT);
+        token = IERC20(_token);
+        seller = msg.sender;
+        tokenId = _tokenId;
+        maxBid = initialBid;
+        maxBidder = _seller;
+        end = _end;
+        NFT.transferFrom(msg.sender, address(this), tokenId);
+        
+    }
+
+    function suggest(uint amount) public {
+
+        require(start && block.timestamp <= end);
+        require(amount > maxBid, "the bid is lower than the max");
+
+        token.transferFrom(msg.sender, address(this), amount);
+        
+        if(maxBidder != seller) {
+            token.transfer(address(maxBidder), maxBid);
+        }
+        
+        maxBid = amount;
+        maxBidder = msg.sender;
+    }
+
+    function endAuction() public {
+
+        require(block.timestamp > end);
+
+        start = false;
+        NFT.transferFrom(address(this), maxBidder, tokenId);
+        token.transfer(seller, maxBid);
+    }
+    
+}
---
+++ test/auction/auction.sol
@@ -0,0 +1,17 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity >=0.6.12 <0.9.0;
+
+// import "forge-std/Test.sol";
+// import "forge-std/console.sol";
+// import "@hack/staking/erc20.sol";
+// import "@hack/auction/erc20.sol";
+// import "@hack/auction/auction.sol";
+
+// contract AuctionTest is Test {
+//     IERC721 NFT;
+//     Auction a;
+
+//     function setUp(address ) {
+
+//     }
+// }
